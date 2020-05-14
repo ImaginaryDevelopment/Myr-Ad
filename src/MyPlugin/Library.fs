5d6bb2f859166933a8e19c21123a737ea0bb7ab7
ImaginaryDevelopment <bdimpe@gmail>
2020-05-14 12:18:53 -0400

    almost back to building

--- src/MyPlugin/Library.fs
+++ src/MyPlugin/Library.fs
@@ -1,5 +1,22 @@
 ﻿namespace MyPlugin
+open Myriad.Sdk
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+[<MyriadGenerator("example1")>]
+type Example1Gen() =
+    interface IMyriadGenerator with
+        member __.Generate(namespace', _ast) =
+
+            let let42 =
+                SynModuleDecl.CreateLet
+                    [ { SynBindingRcd.Let with
+                            Pattern = SynPatRcd.CreateLongIdent(LongIdentWithDots.CreateString "fourtyTwo", [])
+                            Expr = SynExpr.CreateConst(SynConst.Int32 42) } ]
+
+            let componentInfo = SynComponentInfoRcd.Create [ Ident.Create "example1" ]
+            let nestedModule = SynModuleDecl.CreateNestedModule(componentInfo, [ let42 ])
+
+            let namespaceOrModule =
+                { SynModuleOrNamespaceRcd.CreateNamespace(Ident.CreateLong namespace')
+                    with Declarations = [ nestedModule ] }
+
+            namespaceOrModule
