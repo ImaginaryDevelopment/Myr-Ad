725bd39a82aa806075c0b2568ef101024089c738
ImaginaryDevelopment <bdimpe@gmail>
2020-05-14 12:05:36 -0400

    reorganize to prepare for plugins

---
+++ src/Plugs.fs
@@ -0,0 +1,22 @@
+namespace Example
+open Myriad.Sdk
+
+[<MyriadGenerator("example1")>]
+type Example1Gen() =
+    interface IMyriadGenerator with
+        member __.Generate(namespace', _ast) =
+
+            let let42 =
+                SynModuleDecl.CreateLet
+                    [ { SynBindingRcd.Let with
+                            Pattern = SynPatRcd.CreateLongIdent(LongIdentWithDots.CreateString "fourtyTwo", [])
+                            Expr = SynExpr.CreateConst(SynConst.Int32 42) } ]
+
+            let componentInfo = SynComponentInfoRcd.Create [ Ident.Create "example1" ]
+            let nestedModule = SynModuleDecl.CreateNestedModule(componentInfo, [ let42 ])
+
+            let namespaceOrModule =
+                { SynModuleOrNamespaceRcd.CreateNamespace(Ident.CreateLong namespace')
+                    with Declarations = [ nestedModule ] }
+
+            namespaceOrModule
